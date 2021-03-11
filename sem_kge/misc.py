902a0c5e56f18bc799ecdc062b8897a619a668fa
s.f.schouten <sfschouten@gmail.com>
2021-03-11 10:06:38 +0100

    init adagrad state

--- sem_kge/misc.py
+++ sem_kge/misc.py
@@ -15,7 +15,11 @@ def add_constraints_to_job(job, mdmm_module):
     
     # TODO: remove everything below once fixed in pytorch
     if isinstance(job.optimizer, Adagrad):
-        pass #TODO
+        init_acc_val = job.optimizer.defaults['initial_accumulator_value']
+        for p in lambdas + slacks:
+            state = job.optimizer.state[p]
+            state['step'] = 0
+            state['sum'] = torch.full_like(p, init_acc_val, memory_format=torch.preserve_format)
     scheduler = job.kge_lr_scheduler._lr_scheduler
     if isinstance(scheduler, ReduceLROnPlateau):
         scheduler.min_lrs += [scheduler.min_lrs[0]] * 2
