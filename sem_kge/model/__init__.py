afe26d80f4da0d092e7c19df9ac1e0a9e186f9e1
s.f.schouten <sfschouten@gmail.com>
2021-03-19 20:32:55 +0100

    move side-losses to penalty method

--- sem_kge/model/__init__.py
+++ sem_kge/model/__init__.py
@@ -2,7 +2,7 @@ from sem_kge.model.embedder.discrete_stochastic_embedder import DiscreteStochast
 from sem_kge.model.embedder.transt_embedder import TransTEmbedder
 from sem_kge.model.embedder.type_attentive_embedder import TypeAttentiveEmbedder
 
-from sem_kge.model.embedder.loc_scale_embedder import LocScaleEmbedder
+from sem_kge.model.embedder.gaussian_embedder import GaussianEmbedder
 from sem_kge.model.embedder.iaf_embedder import IAFEmbedder
 from sem_kge.model.embedder.type_prior_embedder import TypePriorEmbedder
 
